506dadb784eb13fa3096d90c825a6392b3012806
NagoColer <nagocoler@qq.com>
2023-10-23 19:18:47 +0800

    完成帖子详情前端部分

--- my-project-frontend/src/views/forum/TopicList.vue
+++ my-project-frontend/src/views/forum/TopicList.vue
@@ -21,6 +21,7 @@ import {useStore} from "@/store";
 import axios from "axios";
 import ColorDot from "@/components/ColorDot.vue";
 import router from "@/router";
+import TopicTag from "@/components/TopicTag.vue";
 
 const store = useStore()
 
@@ -44,12 +45,6 @@ watch(() => topics.type, () => resetList(), {immediate: true})
 const today = computed(() => {
     const date = new Date()
     return `${date.getFullYear()} 年 ${date.getMonth() + 1} 月 ${date.getDate()} 日`
-})
-get('/api/forum/types', data => {
-    const array = []
-    array.push({name: '全部', id: 0, color: 'linear-gradient(45deg, white, red, orange, gold, green, blue)'})
-    data.forEach(d => array.push(d))
-    store.forum.types = array
 })
 get('/api/forum/top-topic', data => topics.top = data)
 function updateList(){
@@ -112,7 +107,7 @@ navigator.geolocation.getCurrentPosition(position => {
                 </div>
             </light-card>
             <light-card style="margin-top: 10px;display: flex;flex-direction: column;gap: 10px">
-                <div v-for="item in topics.top" class="top-topic">
+                <div v-for="item in topics.top" class="top-topic" @click="router.push(`/index/topic-detail/${item.id}`)">
                     <el-tag type="info" size="small">置顶</el-tag>
                     <div>{{item.title}}</div>
                     <div>{{new Date(item.time).toLocaleDateString()}}</div>
@@ -147,14 +142,7 @@ navigator.geolocation.getCurrentPosition(position => {
                                 </div>
                             </div>
                             <div style="margin-top: 5px">
-                                <div class="topic-type"
-                                     :style="{
-                                        color: store.findTypeById(item.type)?.color + 'EE',
-                                        'border-color': store.findTypeById(item.type)?.color + '77',
-                                        'background': store.findTypeById(item.type)?.color + '33'
-                                    }">
-                                    {{store.findTypeById(item.type)?.name}}
-                                </div>
+                                <topic-tag :type="item.type"/>
                                 <span style="font-weight: bold;margin-left: 7px">{{item.title}}</span>
                             </div>
                             <div class="topic-content">{{item.text}}</div>
@@ -283,15 +271,6 @@ navigator.geolocation.getCurrentPosition(position => {
         -webkit-line-clamp: 3;
         overflow: hidden;
         text-overflow: ellipsis;
-    }
-
-    .topic-type {
-        display: inline-block;
-        border: solid 0.5px grey;
-        border-radius: 3px;
-        font-size: 12px;
-        padding: 0 5px;
-        height: 18px;
     }
 
     .topic-image {
