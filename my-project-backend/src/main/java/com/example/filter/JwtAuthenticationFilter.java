3514aba92a9b95a78c7bd8ce9b463fd420593246
Nomadic4u <2910887794@qq.com>
2024-06-05 17:41:04 +0800

    修复部分bug

--- my-project-backend/src/main/java/com/example/filter/JwtAuthenticationFilter.java
+++ my-project-backend/src/main/java/com/example/filter/JwtAuthenticationFilter.java
@@ -33,14 +33,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader("Authorization");
         DecodedJWT jwt = utils.resolveJwt(authorization);
-        if(jwt != null) {
+        if (jwt != null) {
+            // 1. 从 JWT 中解析出用户详细信息
             UserDetails user = utils.toUser(jwt);
+            // 2. 创建一个用户名和密码身份验证令牌，包含用户详细信息和权限
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            // 3. 设置请求详细信息到身份验证令牌
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // 4. 将身份验证信息设置到安全上下文中
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            // 5. 将用户ID存放到请求对象属性中，方便后续使用
             request.setAttribute(Const.ATTR_USER_ID, utils.toId(jwt));
         }
+
         filterChain.doFilter(request, response);
     }
 }
